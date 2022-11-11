a86d7a09c7a49a66a69d484cf093a9fc8a05ca10
phuongln6 <phuongln6@fsoft.com.vn>
2022-11-11 17:09:43 +0700

    Fix: dataset pipeline

--- scripts/train_online.sh
+++ scripts/train_online.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 python src/train_online.py \
---exp-name train-resnet50-bsz128-lr1e3 --project_name visual-search \
+--exp-name train-online-batch-hard_resnet50-bsz4-lr1e4 --project_name visual-search \
 --backbone resnet50 \
---batch_size 4 --base_lr 1e-3 \
+--loss batch_hard \
+--batch_size 4 --base_lr 1e-4 \
 --weight_decay 1e-4 \
 --momentum 0.9 
