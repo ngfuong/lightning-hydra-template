9ee5b8936a32f137436d163132cb066d8689f045
ngfuong <lenguyenphuong2204@gmail.com>
2022-11-27 12:44:29 +0700

    Change scripts

--- scripts/train_online_hard.sh
+++ scripts/train_online_hard.sh
@@ -2,6 +2,6 @@ python src/train_online.py \
 --exp-name train-semiloss-batch_hard-resnet50-bsz1-lr1e4-decay1e-5 --project_name visual-search \
 --backbone resnet50 \
 --loss batch_hard \
---batch_size 1 --base_lr 1e-4 \
+--batch_size 2 --base_lr 1e-4 \
 --weight_decay 1e-5 \
 --momentum 0.9 
