ba37dc94fe501e2c871d18047f063175bc397da9
ngfuong <lenguyenphuong2204@gmail.com>
2022-12-14 09:57:32 +0700

    fix: added utils.py file in src

---
+++ src/utils/utils.py
@@ -0,0 +1,11 @@
+import random
+
+def random_exclusion(start, stop, excluded) -> int:
+    """Function for getting a random number with some numbers excluded"""
+    excluded = set(excluded)
+    value = random.randint(start, stop - len(excluded))  # Or you could use randrange
+    for exclusion in tuple(excluded):
+        if value < exclusion:
+            break
+        value += 1
+    return value
